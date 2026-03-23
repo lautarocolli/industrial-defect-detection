3e429d58f02030275158eff52549f8bb5f67d5c2
lautarocolli <collilautaro00@gmail.com>
2026-03-23 02:28:28 +0000

    Created model.py to load the model. The function inside will be called by main.py

---
+++ deployment/app/model.py
@@ -0,0 +1,20 @@
+# app/model.py
+import torch
+from huggingface_hub import hf_hub_download
+from src.models.cnn.resnet import build_resnet
+
+def load_model(device):
+    # Download weights from Hub at startup
+    path = hf_hub_download(
+        repo_id  = "lautivuelos/neu-det-defect-detection",
+        filename = "resnet50_final.pth"
+    )
+
+    checkpoint = torch.load(path, map_location=device)
+
+    model = build_resnet(num_classes=6)
+    model.load_state_dict(checkpoint["model_state_dict"])
+    model.eval()
+    model.to(device)
+
+    return model, checkpoint["classes"]
